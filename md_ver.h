5062359e52b8ce0312683f6dc179baae148a8d90
Jiri Brozovsky <jiri.brozovsky@gmail.com>
2016-07-10 19:10:24 +0200

    new file for version number

---
+++ md_ver.h
@@ -0,0 +1,32 @@
+/*
+   File name: md_ver.h
+   Date:      2016/07/10 17:24
+   Author:    
+
+   Copyright (C) 2016 Jiri Brozovsky
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public License as
+   published by the Free Software Foundation; either version 2 of the
+   License, or (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful, but
+   WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   in a file called COPYING along with this program; if not, write to
+   the Free Software Foundation, Inc., 675 Mass Ave, Cambridge, MA
+   02139, USA.
+*/
+
+
+#ifndef __MD_VER_H__
+#define __MD_VER_H__
+#define MD_VERSION "MicroDef 0.1.6"
+
+
+#endif
+
+/* end of md_ver.h */
